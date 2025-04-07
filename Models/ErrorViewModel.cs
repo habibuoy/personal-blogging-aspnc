f6dac0b6f9d6e71f3425b0a9d1ae4ea1179076a9
habibuoy <habibiemuhammad90@gmail.com>
2025-04-07 15:17:32 +0700

    Base project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PersonalBlogging.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
