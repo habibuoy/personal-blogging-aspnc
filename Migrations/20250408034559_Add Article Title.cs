c808d15a12a3eb114eb361935492cf133d5f6dd9
habibuoy <habibiemuhammad90@gmail.com>
2025-04-08 10:49:04 +0700

    Add Article's title

---
+++ Migrations/20250408034559_Add Article Title.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalBlogging.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddArticleTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorName",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Articles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorName",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- Models/Article.cs
+++ Models/Article.cs
@@ -5,7 +5,8 @@ public class Article
     public int Id { get; set; }
     public DateTime? PublishedDate { get; set; }
     public DateTime? LastModifiedDate { get; set; }
-    public string? AuthorName { get; set; }
+    public string Title { get; set; }
+    public string AuthorName { get; set; }
     public string? Content { get; set; }
     public string[]? Tags { get; set; }
 }
